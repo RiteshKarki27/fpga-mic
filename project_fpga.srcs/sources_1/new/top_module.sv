31c9bb56643ceea0dec554db88ba08e4c00baee7
Ritesh Karki <riteshkarki6@gmail.com>
2025-04-23 15:49:54 +0200

    Add top module for synthesis

---
+++ project_fpga.srcs/sources_1/new/top_module.sv
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/23/2025 03:40:06 PM
+// Design Name: 
+// Module Name: top_module
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module top_module(
+
+    );
+    
+    i2s_to_pcm i2s();
+    loudness_top lt();
+    
+endmodule
