39c49d06370048b675a00ac2dc0e084308e8446e
Ritesh Karki <riteshkarki6@gmail.com>
2025-04-23 17:05:55 +0200

    synthesize

--- project_fpga.srcs/sources_1/new/top_module.sv
+++ project_fpga.srcs/sources_1/new/top_module.sv
@@ -18,13 +18,36 @@
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
-
+//import loudness_meter_pkg::*;
 
 module top_module(
-
+    input var logic clk, //system clk
+	input var logic arstn,  // asynchronous reset
+	input var logic data,
+	output var logic data_ready,
+	output var logic [loudness_meter_pkg::NLEDS-1:0] led
     );
     
-    i2s_to_pcm i2s();
-    loudness_top lt();
+    logic [15:0] tdata;
+    logic tvalid;
+    logic ws, sel;
+    
+    i2s_to_pcm i2s(
+         .clk(clk),
+         .arstn(arstn),
+         .data(data),
+         .lrclk(ws),
+         .sel(sel),
+         .tdata_pcm(tdata),
+         .tvalid_pcm(tvalid));
+         
+    loudness_top lt(
+          .arstn(arstn),
+          .clk(ws),
+          .data(tdata),
+          .data_valid(tvalid),
+          .data_ready(data_ready),
+          .led(led)
+          );
     
 endmodule
